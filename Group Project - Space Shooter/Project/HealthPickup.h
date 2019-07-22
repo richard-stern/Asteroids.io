b445317dbad7d3c544b41781cb168ead17d31382
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-07-22 14:36:35 +1000

    Added and edited comments in HealthPickup.h

--- Group Project - Space Shooter/Project/HealthPickup.h
+++ Group Project - Space Shooter/Project/HealthPickup.h
@@ -1,4 +1,18 @@
 #pragma once
+//--------------------------------------------------------------------------------
+// HealthPickup class
+// Created by Grant Roberts
+//
+// The item that restores the player's health when the player moves over the item.
+//
+// Functions:
+//			  Contstructor()				Creates the object.
+//			  Destructor()					Cleans up the object during deletion.
+//			  OnCollision(Player* player)	Pickup collides with the player.
+//			  OnCollision(Rock* rock)		Pickup collides with a rock.
+//			  OnCollision(Enemy* enemy)		Pickup collides with an enemy.
+//			  OnCollision(Bullet* bullet)	Pickup collides with a bullet.
+//--------------------------------------------------------------------------------
 
 class Actor;
 class Player;
@@ -9,29 +23,43 @@ class Bullet;
 class HealthPickup : public Actor
 {
 public:
+	//----------------------------------------------------------------------------
 	// Constructor.
+	//----------------------------------------------------------------------------
 	HealthPickup();
 
+	//----------------------------------------------------------------------------
 	// Destructor.
+	//----------------------------------------------------------------------------
 	~HealthPickup();
 
+	//----------------------------------------------------------------------------
 	// On collision with the player.
 	// Params: the player.
+	//----------------------------------------------------------------------------
 	void OnCollision(Player* player);
 
+	//----------------------------------------------------------------------------
 	// On collision with a rock.
 	// Params: the rock.
+	//----------------------------------------------------------------------------
 	void OnCollision(Rock* rock);
 
+	//----------------------------------------------------------------------------
 	// On collision with an enemy.
 	// Params: the enemey.
+	//----------------------------------------------------------------------------
 	void OnCollision(Enemy* enemy);
 
+	//----------------------------------------------------------------------------
 	// On collision with a bullet.
 	// Params: the bullet.
+	//----------------------------------------------------------------------------
 	void OnCollision(Bullet* bullet);
 
 private:
+	//----------------------------------------------------------------------------
 	// Bounce the health pickup.
+	//----------------------------------------------------------------------------
 	void Bounce();
 };
