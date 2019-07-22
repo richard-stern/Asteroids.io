f634b6182734d96ae71b20164305097b0620d63a
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-07-22 15:23:04 +1000

    Fixed code to work with Actor files added

--- Group Project - Space Shooter/Project/Turret.cpp
+++ Group Project - Space Shooter/Project/Turret.cpp
@@ -1,12 +1,15 @@
 #include "Turret.h"
+#include "Actor.h"
 #include "Input.h"
 #include "Vector2.h"
 #include "Matrix3.h"
+#include "TextureManager.h"
 
-Turret::Turret()
+Turret::Turret() : Actor(_parent->GetPosition())
 {
-	m_pTexture = TextureManager.LoadTexture("./Images/turret.png");
-	m_bWrapAndRespawn = false;
+	// Ask the texture manager to load the turret texture for the turret.
+	TextureManager* textMan;
+	_texture = textMan->LoadTexture("turret.png");
 }
 
 Turret::~Turret()
@@ -22,19 +25,33 @@ void Turret::Update(float deltaTime)
 	// Get the input.
 	Input* input = input->Instance();
 
-	// The position of the mouse.
-	Vector2 mousePos;
-	mousePos.x = input->GetMouseX();
-	mousePos.y = input->GetMouseY();
+	// The position of the mouse as a Vector3.
+	Vector3 mousePos;
+	mousePos.x = (float)input->GetMouseX();
+	mousePos.y = (float)input->GetMouseY();
+
+	// The position of the turret as a Vector3.
+	Vector3 position;
+	position = _localTransform[6];
 
 	// The direction of where the mouse is.
-	Vector2 direction = mousePos - m_m3LocalMatrix[6];
+	Vector3 direction = mousePos - position;
 	direction.normalise();
 
+	// The foward direction of the turret as a Vector2.
+	Vector2 tempFoward;
+	tempFoward.x = direction.x;
+	tempFoward.y = direction.y;
+
+	// The position of the turret as a Vector2.
+	Vector2 tempPos;
+	tempPos.x = position.x;
+	tempPos.y = position.y;
+
 	// Set the rotation of the turret's matrix.
-	m_m3LocalMatrix.SetRotateZ(direction);
+	_localTransform.setRotateZ(tempFoward);
 
 	// If the left mouse button was pressed -> shoot a bullet.
 	if (input->WasMouseButtonPressed(INPUT_MOUSE_BUTTON_LEFT))
-		m_pBulletManager->ShootBullet(m_m3LocalMatrix[6], direction);
+		m_pBulletManager->ShootBullet(tempPos, tempFoward);
 }
