78570b47855ff26a5e827e1158f6f31f8a50c645
Chopper2000 <47731600+Chopper2000@users.noreply.github.com>
2019-07-23 09:59:03 +1000

    updated Star Class

--- Group Project - Space Shooter/Project/Star.cpp
+++ Group Project - Space Shooter/Project/Star.cpp
@@ -5,6 +5,10 @@ Star::Star()
 	m_pTextureManager = TextureManager::Instance();
 	m_bWrapAndRespawn = true;
 	m_pTexture = m_pTextureManager->LoadTexture("star.png");
+
+
+	SetPosition(Vector2(rand() % 1000, rand() % 1000));
+
 }
 
 Star::~Star()
