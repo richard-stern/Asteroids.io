cf875b47664cdbb37833da57716136b3dca4486e
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-07-23 09:44:50 +1000

    Edited comments in Rock.h

--- Group Project - Space Shooter/Project/Rock.h
+++ Group Project - Space Shooter/Project/Rock.h
@@ -6,12 +6,13 @@
 // This is the class for the asteroids that fly around in the game.
 //
 // Functions:
-//			  Constructor()								Create the rock.
-//			  Destructor()								Clean up the rock.
-//			  OnCollision(Player* player)				Pickup collides with the player
-//			  OnCollision(Bullet* bullet)				Pickup collides with a bullet.
-//			  OnCollision(HealthPickup* healthPickup)	Pickup collides with the health pickup.
-//			  OnCollision(Enemy* enemy)					Pickup collides with an enemy.
+//			  Constructor()					Create the rock.
+//			  Destructor()					Clean up the rock (does nothing).
+//			  OnCollision(Player* player)	Rock collides with the player
+//			  OnCollision(Bullet* bullet)	Rock collides with a bullet.
+//			  OnCollision(HealthPickup* healthPickup)...
+//											Rock collides with the health pickup.
+//			  OnCollision(Enemy* enemy)		Rock collides with an enemy.
 //--------------------------------------------------------------------------------
 
 #include "Actor.h"
