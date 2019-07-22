5ecd50ab5506cb8bffec907b466007e9ea0594ac
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-07-22 16:47:06 +1000

    Changed variable calls in HealthPickup.cpp to Hungarian Notation, as they have been changed in GameObject

--- Group Project - Space Shooter/Project/HealthPickup.cpp
+++ Group Project - Space Shooter/Project/HealthPickup.cpp
@@ -6,29 +6,29 @@
 HealthPickup::HealthPickup() : Actor(Vector2(rand(), rand()))
 {
 	// Initialise variables that are going to be needed.
-	_wrapAndRespawn = true;
-	_visible = true;
+	m_bWrapAndRespawn = true;
+	m_bVisible = true;
 
 	// Sets the velocity in each direction to be somewhere between 1 and 10.
-	_velocity.x = rand() % 10 + 1;
-	_velocity.y = rand() % 10 + 1;
+	m_v2Velocity.x = rand() % 10 + 1;
+	m_v2Velocity.y = rand() % 10 + 1;
 
 	// Ask the texture manager to load the health pickup texture for the turret.
 	TextureManager* textMan;
-	_texture = textMan->LoadTexture("Powerup.png");
+	m_pTexture = textMan->LoadTexture("Powerup.png");
 }
 
 HealthPickup::~HealthPickup()
 {
 	// Clean up.
-	_wrapAndRespawn = false;
-	_visible = false;
+	m_bWrapAndRespawn = false;
+	m_bVisible = false;
 }
 
 void HealthPickup::OnCollision(Player* player)
 {
 	// Set visible to false.
-	_visible = false;
+	m_bVisible = false;
 }
 
 void HealthPickup::OnCollision(Rock* rock)
@@ -46,11 +46,11 @@ void HealthPickup::OnCollision(Enemy* enemy)
 void HealthPickup::OnCollision(Bullet* bullet)
 {
 	// Set visible to false.
-	_visible = false;
+	m_bVisible = false;
 }
 
 void HealthPickup::Bounce()
 {
 	// Reverse the direction of the pickup, basic bounce against object.
-	_velocity * -1;
+	m_v2Velocity * -1;
 }
