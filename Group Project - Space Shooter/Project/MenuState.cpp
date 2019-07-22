28a6f7759a1fcc09b72fbab0fe93359016f0e979
Sorry-Im-Salty <47731654+Sorry-Im-Salty@users.noreply.github.com>
2019-07-22 13:49:33 +1000

    Created Menu State Functions.

--- Group Project - Space Shooter/Project/MenuState.cpp
+++ Group Project - Space Shooter/Project/MenuState.cpp
@@ -8,3 +8,19 @@ MenuState::MenuState() {
 MenuState::~MenuState() {
 
 }
+
+void MenuState::Enter() {
+
+}
+
+void MenuState::Update(float deltaTime, StateMachine* p_StateMachine) {
+	m_pStateMachine = p_StateMachine;
+}
+
+void MenuState::Draw(RenderManager* pRenderManager) {
+
+}
+
+void MenuState::Exit() {
+
+}
--- Group Project - Space Shooter/Project/MenuState.h
+++ Group Project - Space Shooter/Project/MenuState.h
@@ -1,8 +1,23 @@
 #pragma once
 #include "BaseState.h"
+
+/*
+	Base State Class
+
+	Made by Thomas
+*/
+
 class MenuState : public BaseState {
 public:
 	MenuState();
 	~MenuState();
+
+	void Enter();
+	void Update(float deltaTime, StateMachine* p_StateMachine);
+	void Draw(RenderManager* pRenderManager);
+	void Exit();
+
+private:
+	StateMachine* m_pStateMachine;
 };
 
