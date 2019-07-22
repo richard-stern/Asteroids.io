bcd8abbc1b0cb8ad3f7cf5882567e96c2cc6d8dc
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-07-22 15:22:33 +1000

    Added and edited comments in Turret.h

--- Group Project - Space Shooter/Project/Turret.h
+++ Group Project - Space Shooter/Project/Turret.h
@@ -1,4 +1,14 @@
 #pragma once
+//--------------------------------------------------------------------------------
+// Turret class
+// Created by Grant Roberts
+//
+// This object is the turret that follows the player, pointing towards the mouse.
+// Will shoot a bullet when the left mouse button is pressed.
+//
+// Functions:
+//			  Update(float deltaTime) Update the turret.
+//--------------------------------------------------------------------------------
 
 #include "BulletManager.h"
 
@@ -7,12 +17,25 @@ class Actor;
 class Turret : public Actor
 {
 public:
+	//----------------------------------------------------------------------------
+	// Constructor.
+	//----------------------------------------------------------------------------
 	Turret();
+
+	//----------------------------------------------------------------------------
+	// Destructor.
+	//----------------------------------------------------------------------------
 	~Turret();
 
+	//----------------------------------------------------------------------------
+	// Update the turret.
+	// Params: delta time.
+	//----------------------------------------------------------------------------
 	void Update(float deltaTime);
 
 private:
+	//----------------------------------------------------------------------------
+	// The bullet manager.
+	//----------------------------------------------------------------------------
 	BulletManager* m_pBulletManager;
 };
-
