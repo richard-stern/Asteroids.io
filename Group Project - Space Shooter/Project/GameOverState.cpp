9c2bfe3ecfafe1ca62e29167dff0e686f1c5269a
Lenammon <40161201+Lenammon@users.noreply.github.com>
2019-07-23 15:09:41 +1000

    added quit button to Game Over State, also added delete of font and nullptr

--- Group Project - Space Shooter/Project/GameOverState.cpp
+++ Group Project - Space Shooter/Project/GameOverState.cpp
@@ -7,15 +7,19 @@
 GameOverState::GameOverState() : BaseState()
 {
 	m_font = new Font("./Fonts/hobo_32px.fnt");
+	
 }
 GameOverState::~GameOverState()
 {
 	// clean up the project
 	delete m_font;
+
 }
 void GameOverState ::Enter()
 {
 	std::cout << "enter game over state" << std::endl;
+	
+
 }
 void GameOverState ::Update(float deltaTime, StateMachine* p_StateMachine)
 {
@@ -32,9 +36,12 @@ void GameOverState ::Draw (RenderManager* pRenderManager)
 	pRenderManager->DrawText(m_font, 0, 400, 500);
 	//play again button
 	pRenderManager->DrawText(m_font, "play again?", 400, 500);
-
+	// quit button
+	pRenderManager->DrawText(m_font, "Quit", 400, 600);
 }
 void GameOverState ::Exit ()
 {
 	std::cout << "exit game over state" << std::endl;
+	delete m_font;
+	m_font = nullptr;
 }
