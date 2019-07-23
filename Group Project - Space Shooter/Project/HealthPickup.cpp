59316b4f9850c4b02c8007f6ef98355fe94ed06b
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-07-24 09:17:32 +1000

    Added comments in HealthPickup.cpp

--- Group Project - Space Shooter/Project/HealthPickup.cpp
+++ Group Project - Space Shooter/Project/HealthPickup.cpp
@@ -2,6 +2,7 @@
 #include <random>
 #include "Actor.h"
 #include "TextureManager.h"
+#include "BoxCollider.h"
 
 HealthPickup::HealthPickup() : Actor(Vector2(100, 100))
 {
@@ -17,9 +18,18 @@ HealthPickup::HealthPickup() : Actor(Vector2(100, 100))
 	TextureManager* textMan = textMan->Instance();
 	m_pTexture = textMan->LoadTexture("Powerup.png");
 
-	m_v2Forward.x = rand() % 180;
-	m_v2Forward.y = rand() % 180;
+	// Set the foward direction to between 1 to 1000.
+	m_v2Forward.x = (float)(rand() % 1000 + 1);
+	m_v2Forward.y = (float)(rand() % 1000 + 1);
 	m_v2Forward.normalise();
+	
+	// Set the velocity to between -3 and 3.
+	m_v2Velocity.x = (float)(rand() % (5 + 1) - 3);
+	m_v2Velocity.y = (float)(rand() % (5 + 1) - 3);
+
+	// Collider
+	//Vector2 v2Extend = Vector2(m_pTexture->GetWidth() / 2, m_pTexture->GetHeight() / 2);
+	//m_pCollider = new BoxCollider(v2Extend);
 }
 
 HealthPickup::~HealthPickup()
@@ -31,16 +41,21 @@ HealthPickup::~HealthPickup()
 
 void HealthPickup::Update(float deltaTime)
 {
+	// Call the base class update.
 	Actor::Update(deltaTime);
 
+	// Get the current position as a Vector3.
 	Vector3 v3Pos = GetPosition();	
 
+	// Convert the current position to a Vector2.
 	Vector2 v2Pos;
 	v2Pos.x = v3Pos.x;
 	v2Pos.y = v3Pos.y;
 
+	// Change the position by the velocity.
 	v2Pos += m_v2Velocity;
 
+	// Set position to the new position.
 	SetPosition(v2Pos);
 }
 
