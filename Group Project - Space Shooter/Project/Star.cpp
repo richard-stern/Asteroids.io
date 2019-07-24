9b504e9c1d62b558e3a91ec8b762a859d921179d
Alex Mollard <alex.mollard@mail.com>
2019-07-24 16:06:09 +1000

    Fixed the stars and left them simple

--- Group Project - Space Shooter/Project/Star.cpp
+++ Group Project - Space Shooter/Project/Star.cpp
@@ -11,14 +11,11 @@ Star::Star()
 	m_bWrapAndRespawn = true;
 
 	m_pStarNormal = m_pTextureManager->LoadTexture("star.png");
-	m_pStarUp = m_pTextureManager->LoadTexture("StarUp.png");
-	m_pStarSide = m_pTextureManager->LoadTexture("StarSide.png");
 
-	m_pTexture = m_pStarSide;
+	m_pTexture = m_pStarNormal;
 	m_eType = GameObjectType::STAR;
 
-	SetPosition(Vector2(float(rand() % Camera::Instance()->GetWindowWidth()), float(rand() % Camera::Instance()->GetWindowHeight())));
-	SetScale(Vector2(2.0f,2.0f));
+	SetPosition(Vector2(float(rand() % Camera::Instance()->GetWindowWidth() * 2), float(rand() % Camera::Instance()->GetWindowHeight() * 2)));
 }
 
 Star::~Star()
@@ -28,23 +25,5 @@ Star::~Star()
 
 void Star::Update(float deltaTime)
 {
-	f_timer += deltaTime;
 	GameObject::Update(deltaTime);
-
-	if (f_timer > 5.0f)
-	{
-		f_timer = 0.0f;
-		if (m_pTexture == m_pStarUp)
-		{
-			m_pTexture = m_pStarSide;
-		}
-		else if (m_pTexture == m_pStarSide)
-		{
-			m_pTexture = m_pStarNormal;
-		}
-		else if (m_pTexture == m_pStarNormal)
-		{
-			m_pTexture = m_pStarUp;
-		}
-	}
 }
