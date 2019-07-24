d2053a83026a6dc1a2f9dd622e4cd157c854324a
SethJPistol <47731608+SethJPistol@users.noreply.github.com>
2019-07-24 11:05:59 +1000

    Added parent-setting for bullets in the bullet manager - We can shoot bullets now!

--- Group Project - Space Shooter/Project/BulletManager.cpp
+++ Group Project - Space Shooter/Project/BulletManager.cpp
@@ -1,28 +1,31 @@
 #include "BulletManager.h"
 #include "Bullet.h"
 #include "Level.h"
+#include "Blackboard.h"
 
 BulletManager::BulletManager()
 {
+	Level* pLevel = Blackboard::Instance()->GetLevel();
+
 	for (int i = 0; i < BULLET_POOL_SIZE; ++i)	//Loop and create however many bullets
 	{
 		m_aBulletPool.push_back(new Bullet());	//Add a bullet to the pool
-		//pLevel->AddChild(m_aBulletPool[i]);
+		pLevel->AddChild(m_aBulletPool[i]);
 	}
 }
 
 
 BulletManager::~BulletManager()
 {
-	//Destroy the bullet pool
-	for (int i = 0; i < BULLET_POOL_SIZE; ++i)	//For all bullets in the pool,
-	{
-		if (m_aBulletPool[i])					//If the bullet exists,
-		{
-			delete m_aBulletPool[i];			//Delete each bullet
-			m_aBulletPool[i] = nullptr;			//Set to nullptr
-		}
-	}
+	////Destroy the bullet pool
+	//for (int i = 0; i < BULLET_POOL_SIZE; ++i)	//For all bullets in the pool,
+	//{
+	//	if (m_aBulletPool[i])					//If the bullet exists,
+	//	{
+	//		delete m_aBulletPool[i];			//Delete each bullet
+	//		m_aBulletPool[i] = nullptr;			//Set to nullptr
+	//	}
+	//}
 }
 
 void BulletManager::ShootBullet(Vector2 v2StartPos, Vector2 v2StartDir)
